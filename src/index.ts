be6ff41a5a5ad682d8c0a39be87b8df7177f6f3d
evandikt <evandikt@gmail.com>
2021-09-10 15:35:28 +0200

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import process from 'process'
-import { getEvntComClientFromChildProcess, getEvntComServerFromChildProcess } from 'evntboard-communicate'
 // @ts-ignore
 import { default as io } from "socket.io-client";
 
@@ -7,19 +5,25 @@ import {
   EStreamElementsEvent,
   EStreamElementsEventType,
 } from "./EStreamElementsEvent";
+import {EvntComNode} from "evntcom-js/dist/node";
 
-// parse params
-const { name: NAME, customName: CUSTOM_NAME, config: { token: TOKEN } } = JSON.parse(process.argv[2])
-const EMITTER = CUSTOM_NAME || NAME
+const NAME: string = process.env.EVNTBOARD_NAME || "streamelements";
+const HOST: string = process.env.EVNTBOARD_HOST || "localhost";
+const PORT: number = process.env.EVNTBOARD_PORT ? parseInt(process.env.EVNTBOARD_PORT) : 5001;
+const TOKEN: string = process.env.EVNTBOARD_CONFIG_TOKEN;
 
-const evntComClient = getEvntComClientFromChildProcess();
-const evntComServer = getEvntComServerFromChildProcess();
+const evntCom = new EvntComNode({
+  name: NAME,
+  port: PORT,
+  host: HOST,
+});
+
 
 let socket: any;
 let attemps: number = 0;
 
-const onNewEvent = (data: any): void => {
-  if (data?.emitter !== EMITTER) return
+evntCom.onEvent = (data: any): void => {
+  if (data?.emitter !== NAME) return
   switch (data?.event) {
     case EStreamElementsEvent.OPEN:
       attemps = 0
@@ -33,39 +37,40 @@ const onNewEvent = (data: any): void => {
 }
 
 const load = async () => {
-  evntComClient?.newEvent(EStreamElementsEvent.LOAD, null, { emitter: EMITTER });
-  socket = io("https://realtime.streamelements.com", {
-    transports: ["websocket"],
+  await evntCom.callMethod("newEvent", [EStreamElementsEvent.LOAD, null, { emitter: NAME }]);
+  socket = io('https://realtime.streamelements.com', {
+    transports: ['websocket']
   });
 
-  const eventHandler = ({ listener, ...rest }: any) => {
+  const eventHandler = async ({ listener, provider, type, ...rest }: any) => {
     if (Object.values(EStreamElementsEventType).includes(listener)) {
-      evntComClient?.newEvent(
+      await evntCom.callMethod("newEvent", [
           listener,
-          {listener, ...rest},
-          { emitter: EMITTER }
-      );
+          {listener, provider, type, ...rest},
+          { emitter: NAME }
+      ]);
     } else {
-      console.warn(`Unknow event on streamelements ${listener}`);
+      await evntCom.callMethod("newEvent", [
+          `streamelements-${provider}-${type}`,
+          {listener, provider, type, ...rest},
+          { emitter: NAME }
+      ]);
     }
   }
 
   // Socket connected
   socket.on("connect", () => {
-    socket.emit("authenticate", {
-      method: "jwt",
-      token: TOKEN,
-    });
+    socket.emit('authenticate', {method: 'jwt', token: TOKEN});
   });
 
   // Socket got disconnected
   socket.on("disconnect", (data: any) => {
-    evntComClient?.newEvent(EStreamElementsEvent.CLOSE, data, { emitter: EMITTER });
+    evntCom.callMethod("newEvent", [EStreamElementsEvent.CLOSE, data, { emitter: NAME }]);
   });
 
   // Socket is authenticated
   socket.on("authenticated", () => {
-    evntComClient?.newEvent(EStreamElementsEvent.OPEN, { emitter: EMITTER });
+    evntCom.callMethod("newEvent", [EStreamElementsEvent.OPEN, {}, { emitter: NAME }]);
   });
 
   socket.on("event", (data: any) => {
@@ -85,28 +90,11 @@ const load = async () => {
   });
 }
 
-const unload = async () => {
-  try {
-    socket.disconnect();
-    evntComClient?.newEvent(EStreamElementsEvent.UNLOAD, null, { emitter: EMITTER });
-  } catch (e) {
-    console.error(e.stack);
-  }
-}
-
-const reload = async () => {
-  await unload();
-  await load();
-}
-
 const tryReconnect = () => {
   attemps += 1
-  console.log(`Attempt to reconnect OBS for the ${attemps} time(s)`)
+  console.log(`Attempt to reconnect STREAMELEMENTS for the ${attemps} time(s)`)
   const waintingTime = attemps * 5000
   setTimeout(async () => await load(), waintingTime)
 }
 
-evntComServer.expose('newEvent', onNewEvent)
-evntComServer.expose('load', load)
-evntComServer.expose('unload', unload)
-evntComServer.expose('reload', reload)
+evntCom.onOpen = load;
